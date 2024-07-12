bb420cb0b9df222e81fe1fb9f8c71775d545ae53
Ahmed Soliman <ahmed.h.soliman@outlook.com>
2024-07-11 22:56:14 -0400

    wip client connection logic

---
+++ client/client.go
@@ -0,0 +1,81 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"go-axon/internal/pb"
+	"google.golang.org/grpc"
+)
+
+type AxonClient interface{}
+
+// Options for the configuration of the Axon client.
+type Options struct {
+	// Address is the axon server address to connect to.
+	Host string
+
+	// Port is the port of the axon server to connect to.
+	Port int
+
+	// ComponentName is the name of the component. Several instances of the same component should share this name.
+	ComponentName string
+
+	// ClientId is the unique identifier of the client. Is used to distinguish different instances of the same component.
+	ClientId string
+
+	// Tags are optional and can be used to provide hints and preferences for setting up connections.
+	Tags map[string]string
+
+	// Version is the Axon Framework version of the client.
+	Version string
+}
+
+func Dial(ctx context.Context, options Options) (AxonClient, error) {
+	address := fmt.Sprintf("%s:%d", options.Host, options.Port)
+	grpcConnection, err := grpc.NewClient(address)
+	if err != nil {
+		return nil, err
+	}
+
+	platformClient := pb.NewPlatformServiceClient(grpcConnection)
+	platform, err := platformClient.GetPlatformServer(ctx, &pb.ClientIdentification{
+		ClientId:      options.ClientId,
+		ComponentName: options.ComponentName,
+		Tags:          options.Tags,
+		Version:       options.Version,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if platform.SameConnection {
+		return &axonClient{
+			grpcConnection: grpcConnection,
+			commandClient:  pb.NewCommandServiceClient(grpcConnection),
+			eventClient:    pb.NewEventStoreClient(grpcConnection),
+		}, nil
+	}
+
+	err = grpcConnection.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	address = fmt.Sprintf("%s:%d", platform.Primary.HostName, platform.Primary.GrpcPort)
+	nodeGrpcConnection, err := grpc.NewClient(address)
+	if err != nil {
+		return nil, err
+	}
+
+	return &axonClient{
+		grpcConnection: nodeGrpcConnection,
+		commandClient:  pb.NewCommandServiceClient(nodeGrpcConnection),
+		eventClient:    pb.NewEventStoreClient(nodeGrpcConnection),
+	}, nil
+}
+
+type axonClient struct {
+	grpcConnection *grpc.ClientConn
+	commandClient  pb.CommandServiceClient
+	eventClient    pb.EventStoreClient
+}
